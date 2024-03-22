4bd392d08aa92cafa91fdcfaefa7232fe575d6ed
Akshaj <akshaj.kanumuri@gmail.com>
2024-03-22 01:14:59 -0700

    update

---
+++ player.gd
@@ -0,0 +1,70 @@
+extends CharacterBody3D
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 7.0
+const JUMP_VELOCITY = 4.5
+
+@export var MOUSE_SENS = 0.005
+
+var gravity = 20
+
+const BOB_FREQ = 2.5
+const BOB_AMP = 0.08
+var t_bob = 0.0
+
+var speed = WALK_SPEED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENS)
+		head.rotate_x(-event.relative.y * MOUSE_SENS)
+		head.rotation.x = clamp(head.rotation.x, -PI/2, PI/2)
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "front", "back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	if direction:
+		camera.transform.origin = _headbob(t_bob)
+	else:
+		camera.transform.origin = Vector3.ZERO
+	
+	print(transform.origin)
+	
+	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	return pos
